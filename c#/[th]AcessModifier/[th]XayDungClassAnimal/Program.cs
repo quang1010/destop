464af221a24fd4de216c27ccb61901319f6fccf6
Quang.nguyendinh1010 <quang.nguyendinh1010@gmail.com>
2020-06-18 04:37:15 +0700

    .

---
+++ c#/[th]AcessModifier/[th]XayDungClassAnimal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _th_XayDungClassAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "Kitty");
+            cat.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
